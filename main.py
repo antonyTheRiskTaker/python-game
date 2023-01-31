af625daa652a6903ca9847d367b282deb20a655b
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-01-31 11:34:07 +0800

    added extra branches to finish the game prototype

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 answer = input("Would you like to play? (yes/no) > ").lower().strip()
 
 if answer == 'yes':
-    answer = input('You reach a crossroads, would you like to go left or right? (left/right) > ').lower().strip()
+    answer = input('You reached a crossroads, would you like to go left or right? (left/right) > ').lower().strip()
     if answer == 'left':
-        answer = input('You encounter a monster, would you like to run or attack? (run/attack) > ').lower().strip()
+        answer = input('You encountered a monster, would you like to run or attack? (run/attack) > ').lower().strip()
         if answer == 'attack':
             print('You were fighting bare-handed. You were dead.')
         else:
             print('Good choice, you made it away safely.')
+
+            answer = input('You saw a car and a plane. Which would like to take? (car/plane) > ')
+
+            if answer == 'plane':
+                print('Unfortunately you did not know how to fly a plane and it crashed soon after it took off... Game over.')
+            else:
+                print('You found a stash of gold bars in the boot of the car. You drove off the place as a millionaire.')
     elif answer == 'right':
         print('You walked aimlessly to the right and fell on a patch of ice. You injured your leg and cannot continue. Game over!')
     else:
