d16fff179ea449cc13638f1c4e9b1e56500be562
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-03-25 22:23:19 +0800

    change bg colour and add an image

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 class Settings:
     """A class to store all settings for Alien Invasion."""
+    GREY_COLOR = (230, 230, 230)
+    BLUE_COLOR = (0, 100, 255)
 
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = Settings.BLUE_COLOR
