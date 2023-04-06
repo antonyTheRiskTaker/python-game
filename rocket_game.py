036597447729ff42e7ef68c27f218e085d3671a9
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-04-06 23:08:38 +0800

    make a new rocket game class

---
+++ rocket_game.py
@@ -0,0 +1,19 @@
+import sys
+import pygame
+
+
+class RocketGame:
+    """
+    Try It Yourself Exercise 12-4. Overall class to manage game assets and
+    behaviour.
+    The game needs to fulfil three requirements:
+    1. It position a rocket in the centre of the screen.
+    2. The player can move the rocket up, down, left or right using the four 
+       arrow keys.
+    3. The rocket never moves beyond any edge of the screen.
+    """
+
+    def __init__(self):
+        """Initialise the game, and create game resources."""
+        # TODO: continue from here
+        pass
