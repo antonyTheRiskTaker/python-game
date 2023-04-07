2e70e2aea6e2935d6ca366ea7d52a81c82d730b6
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-04-07 13:55:37 +0800

    add settings class for rocket game
    Rocket game settings initialised.

---
+++ rocket_game_settings.py
@@ -0,0 +1,9 @@
+class RocketGameSettings:
+    """A class to store all settings for Rocket Game."""
+
+    def __init__(self):
+        """Initialise the game's settings."""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (230, 230, 230)
+        
