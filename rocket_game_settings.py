4d6ad3fa5365add8632154af56686bdf715c3d6c
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-04-14 01:43:10 +0800

    add rocket speed attribute

--- rocket_game_settings.py
+++ rocket_game_settings.py
@@ -6,4 +6,6 @@ class RocketGameSettings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_colour = (230, 230, 230)
-        
+
+        # Rocket settings
+        self.rocket_speed = 1.5
