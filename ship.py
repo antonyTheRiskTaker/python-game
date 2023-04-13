ee68e7107b23737ae0687e1ed48ac60a433f956e
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-04-14 01:18:18 +0800

    reformat ship class

--- ship.py
+++ ship.py
@@ -1,11 +1,12 @@
 import pygame
 
+
 class Ship:
     """A class to manage the ship."""
 
     def __init__(self, ai_game):
         """Initialise the ship and set its starting position."""
-        
+
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
@@ -31,10 +32,10 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-        
+
         # Update rect object from self.x.
         self.rect.x = self.x
-    
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
