20acecac17372edf92ff1f94784992c1c184eaec
antonyTheRiskTaker <mrmikadox@gmail.com>
2023-04-18 23:17:07 +0800

    set up a new file for try it yourself 12-5 ex

---
+++ event_screen.py
@@ -0,0 +1,11 @@
+import sys
+import pygame
+
+
+class EventScreen:
+    """Try It Yourself Exercise 12-5. Overall class to manage screen assets and behaviour.
+    Fulfil these requirements to finish this exercise:
+    1. Create an empty screen.
+    2. Print the `event.key` attribute whenever a `pygame.KEYDOWN` event is detected.
+    3. Run the program and press various keys to see how Pygame responds."""
+    pass
